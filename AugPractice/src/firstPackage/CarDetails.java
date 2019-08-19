8b428698c263b10019e19ca1be813dac8a9998d5
Deepu <Deepu@192.168.0.18>
2019-08-19 01:15:12 -0400

    Commit 2

--- AugPractice/src/firstPackage/CarDetails.java
+++ AugPractice/src/firstPackage/CarDetails.java
@@ -1,40 +1,40 @@
-package firstPackage;
-
-public class CarDetails {
-	
-	public static void main(String[] args) {
-		
-		CarModels q7 = new CarModels();
-		CarModels qx60 = new CarModels();
-		CarModels mdx = new CarModels();
-		
-		q7.make = "Audi";
-		q7.model = "Q7";
-		q7.km = 123555;
-		q7.accident = true;
-		q7.year = 2011;
-		q7.testEligibilityForCPO();
-		
-		qx60.make = "Infinity";
-		qx60.model = "QX60";
-		qx60.km = 78000;
-		qx60.accident = false;
-		qx60.year = 2015;
-		qx60.testEligibilityForCPO();
-		
-		mdx.make = "Acura";
-		mdx.model = "MDX";
-		mdx.km = 7000;
-		mdx.accident = true;
-		mdx.year = 2019;
-		mdx.testEligibilityForCPO();
-		
-		System.out.println("Is Q7 eligible for CPO? " + q7.eligibleForCPO);
-		System.out.println("Is QX60 eligible for CPO? " + qx60.eligibleForCPO);
-		System.out.println("Is MDX eligible for CPO? " + mdx.eligibleForCPO);
-		
-		
-		
-	}
-
-}
+//package firstPackage;
+//
+//public class CarDetails {
+//	
+//	public static void main(String[] args) {
+//		
+//		CarModels q7 = new CarModels();
+//		CarModels qx60 = new CarModels();
+//		CarModels mdx = new CarModels();
+//		
+//		q7.make = "Audi";
+//		q7.model = "Q7";
+//		q7.km = 123555;
+//		q7.accident = true;
+//		q7.year = 2011;
+//		q7.testEligibilityForCPO();
+//		
+//		qx60.make = "Infinity";
+//		qx60.model = "QX60";
+//		qx60.km = 78000;
+//		qx60.accident = false;
+//		qx60.year = 2015;
+//		qx60.testEligibilityForCPO();
+//		
+//		mdx.make = "Acura";
+//		mdx.model = "MDX";
+//		mdx.km = 7000;
+//		mdx.accident = true;
+//		mdx.year = 2019;
+//		mdx.testEligibilityForCPO();
+//		
+//		System.out.println("Is Q7 eligible for CPO? " + q7.eligibleForCPO);
+//		System.out.println("Is QX60 eligible for CPO? " + qx60.eligibleForCPO);
+//		System.out.println("Is MDX eligible for CPO? " + mdx.eligibleForCPO);
+//		
+//		
+//		
+//	}
+//
+//}
--- AugPractice/src/firstPackage/CarModels.java
+++ AugPractice/src/firstPackage/CarModels.java
@@ -1,18 +1,18 @@
-package firstPackage;
-
-public class CarModels {
-	
-	String make;
-	String model;
-	int year;
-	float km;
-	boolean accident;
-	boolean eligibleForCPO;
-	
-	public void testEligibilityForCPO(){
-		if (year >= 2014 & km < 100000 & !accident) {
-			eligibleForCPO=true;
-		}
-	}
-
-}
+//package firstPackage;
+//
+//public class CarModels {
+//	
+//	String make;
+//	String model;
+//	int year;
+//	float km;
+//	boolean accident;
+//	boolean eligibleForCPO;
+//	
+//	public void testEligibilityForCPO(){
+//		if (year >= 2014 & km < 100000 & !accident) {
+//			eligibleForCPO=true;
+//		}
+//	}
+//
+//}
